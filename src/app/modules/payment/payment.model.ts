d99cacf587d2d05437bc37744e5f1f8fa3fab750
Appon Islam <appon2003@gmail.com>
2026-03-07 00:32:42 +0600

    payment model updated for remove version key

--- src/app/modules/payment/payment.model.ts
+++ src/app/modules/payment/payment.model.ts
@@ -43,7 +43,8 @@ const paymentSchema = new Schema<IPayment>(
     },
     {
         timestamps: true,
-    }
+        versionKey: false,
+    },
 );
 
 export const PaymentModel = model<IPayment>("Payment", paymentSchema);
