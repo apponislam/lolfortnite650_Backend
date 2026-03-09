0395c4396a2b3dc814f1492166a754b62558c66d
Appon Islam <appon2003@gmail.com>
2026-03-09 23:11:06 +0600

    bank details interface updated

--- src/app/modules/bankDetails/bankDetails.interface.ts
+++ src/app/modules/bankDetails/bankDetails.interface.ts
@@ -1,24 +1,25 @@
 import { Types } from "mongoose";
 
 export interface IBankAccount {
-  _id?: string;
+    userId: Types.ObjectId;
 
-  userId: Types.ObjectId;
+    accountHolderName: string;
+    bankName: string;
+    accountNumber: string;
 
-  accountHolderName: string;
-  bankName: string;
-  accountNumber: string;
+    routingNumber?: string;
+    branchName?: string;
 
-  routingNumber?: string;
-  branchName?: string;
+    swiftCode?: string;
+    iban?: string;
+    country?: string;
 
-  swiftCode?: string;
-  iban?: string;
-  country?: string;
+    bankAddress?: string;
+    beneficiaryAddress?: string;
 
-  isVerified: boolean;
-  isDefault: boolean;
+    isVerified: boolean;
+    isDefault: boolean;
 
-  addedAt: Date;
-  updatedAt: Date;
+    addedAt: Date;
+    updatedAt: Date;
 }
