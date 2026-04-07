a08903ffa0725bc552ae134d8c6cc870f6329321
Appon Islam <appon2003@gmail.com>
2026-04-08 05:31:06 +0600

    zoom functionalitily working perfectly

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -15,7 +15,6 @@ import { hourlyClassRoutes } from "../modules/hourlyclasses/hourlyclass.routes";
 import { ClassPaymentRoutes } from "../modules/classpayments/classpayments.routes";
 import { WithdrawRoutes } from "../modules/withdraw/withdraw.routes";
 import { DashboardRoutes } from "../modules/dashboard/dashboard.routes";
-import { zoomRoutes2 } from "../modules/zoom2/zoom.routes";
 
 const router = express.Router();
 
@@ -55,10 +54,6 @@ const moduleRoutes = [
     {
         path: "/zoom",
         route: zoomRoutes,
-    },
-    {
-        path: "/zoom2",
-        route: zoomRoutes2,
     },
 
     {
